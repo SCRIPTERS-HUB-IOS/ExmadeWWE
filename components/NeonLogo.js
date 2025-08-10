5179fdb40b3655e0436d778183d340a55b9ad31e
SCRIPTERS-HUB-IOS <testaccmcc3@gmail.com>
2025-08-10 13:03:29 +0800

    Create NeonLogo.js

---
+++ components/NeonLogo.js
@@ -0,0 +1,7 @@
+export default function NeonLogo() {
+  return (
+    <div className="fixed bottom-4 left-4 neon-text font-bold text-lg select-none pointer-events-none z-50">
+      exmade
+    </div>
+  )
+}
